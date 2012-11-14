01fc876b36e349aa73dd3e238e59ec083ad03bbb
Subramanian Venkatesan <subbu@sub.local>
2012-11-14 13:26:56 -0500

    Added comments in the code

--- rng.py
+++ rng.py
@@ -9,18 +9,24 @@ Language   : Python
 
 import time
 class RandomNumberGenerator:
+    #constructor
     def __init__(self):
         self.randomints=[];
-        self.count=0;
+        self.count=0; # no of random numbers generated in the sequence.
         self.a=1664525
-        self.m=4294967296
+        self.m=4294967296 # 2^32
         self.seed=time.mktime(time.gmtime()) # get epoch
-    def rand(self): #generates a random number between 0 and 1
+    
+    # rand function
+    def rand(self): 
         if self.count==0:
+           # append X1=a*seed mod m
            self.randomints.append((self.a*self.seed)%self.m)
         else:
+	   # append Xn=a*Xn-1 mod m
            self.randomints.append((self.a*self.randomints[-1])%self.m)
-	self.count+=1
+	#increment the count
+	self.count+=1 
         return self.randomints[-1]/self.m #return x/m to get x between 0 and 1
         
 def main():
